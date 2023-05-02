e9e3782bae0e8b2b9eeeb2b9f3ef46159a04ffc2
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-05-01 23:11:25 -0400

    m6a CIMS

--- test/CIMS/m6a.sh
+++ test/CIMS/m6a.sh
@@ -9,3 +9,9 @@ param=$#
 
 # Parsing SAM file
 perl /scr1/users/liuc9/tools/anaconda3/envs/ctk/lib/ctk/parseAlignment.pl -v --map-qual 1 --min-len 18 --mutation-file aligned.sorted.mutation.txt aligned.sorted.sam aligned.sorted.tag.bed
+
+# Remove tags from rRNA and other repetitive RNA (optional)
+perl /scr1/users/liuc9/tools/anaconda3/envs/ctk/lib/ctk/tagoverlap.pl -big -region /scr1/users/liuc9/tools/anaconda3/envs/ctk/lib/ctk/annotation/genomes/hg38/annotation/rmsk.RNA.bed -ss --complete-overlap -r --keep-tag-name --keep-score -v aligned.sorted.tag.bed aligned.sorted.tag.norRNA.bed
+
+# Collapse PCR duplicates
+perl /scr1/users/liuc9/tools/anaconda3/envs/ctk/lib/ctk/tag2collapse.pl -big -v --random-barcode -EM 30 --seq-error-model alignment -weight --weight-in-name --keep-max-score --keep-tag-name aligned.sorted.tag.norRNA.bed aligned.sorted.tag.uniq.bed
