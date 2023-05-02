915c31efc88afdc681013e64fc75d6d4d5e114bc
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-05-02 00:14:35 -0400

    m6a

--- test/CIMS/m6a.sh
+++ test/CIMS/m6a.sh
@@ -15,3 +15,9 @@ perl /scr1/users/liuc9/tools/anaconda3/envs/ctk/lib/ctk/tagoverlap.pl -big -regi
 
 # Collapse PCR duplicates
 perl /scr1/users/liuc9/tools/anaconda3/envs/ctk/lib/ctk/tag2collapse.pl -big -v --random-barcode -EM 30 --seq-error-model alignment -weight --weight-in-name --keep-max-score --keep-tag-name aligned.sorted.tag.norRNA.bed aligned.sorted.tag.uniq.bed
+
+awk '{print $3-$2}' aligned.sorted.tag.uniq.bed | sort -n | uniq -c | awk '{print $2"\t"$1}' >aligned.sorted.tag.uniq.len.dist.txt
+
+python /scr1/users/liuc9/tools/anaconda3/envs/ctk/lib/ctk/joinWrapper.py aligned.sorted.mutation.txt aligned.sorted.tag.uniq.bed 4 4 N aligned.sorted.tag.uniq.mutation.txt
+
+# Merging biological replicates
