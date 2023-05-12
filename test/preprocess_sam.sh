7f9bd1f952d26db0cc2678bb8c1131e9f35880b1
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-05-12 13:56:14 -0400

    complete dataset

--- test/preprocess_sam.sh
+++ test/preprocess_sam.sh
@@ -7,16 +7,32 @@
 # Number of input parameters
 param=$#
 
+# ! for complete bam
+
 conda activate clair3
 
-rawsam=/mnt/isilon/xing_lab/liuc9/projdata/m6a/test4m_result/alignment/M6A/0/aligned.sorted.sam
+rawsam=/mnt/isilon/xing_lab/liuc9/projdata/m6a/APOBEC1_YTH_in_vivo_20230426/alignment/M6A/0/aligned.sorted.sam
 genomeref=/mnt/isilon/xing_lab/liuc9/refdata/bwa_index/Homo_sapiens.GRCh38.104.fa
 
-targetdir=/scr1/users/liuc9/m6a/
-prefix=test4m.sorted.md
+targetdir=/home/liuc9/data/projdata/m6a/mapping
+prefix=APOBEC1YTH.sorted.md
 # Step1: add MD tag to SAM file
 samtools fillmd ${rawsam} ${genomeref} >${targetdir}/${prefix}.sam
 
 # Step2: convert sam file to bam and index bam
 samtools view -bS --threads 10 ${targetdir}/${prefix}.sam -o ${targetdir}/${prefix}.bam
 samtools index -@10 ${targetdir}/${prefix}.bam
+
+# ! for test4m dataset
+
+# rawsam=/mnt/isilon/xing_lab/liuc9/projdata/m6a/test4m_result/alignment/M6A/0/aligned.sorted.sam
+# genomeref=/mnt/isilon/xing_lab/liuc9/refdata/bwa_index/Homo_sapiens.GRCh38.104.fa
+
+# targetdir=/scr1/users/liuc9/m6a/
+# prefix=test4m.sorted.md
+# # Step1: add MD tag to SAM file
+# samtools fillmd ${rawsam} ${genomeref} >${targetdir}/${prefix}.sam
+
+# # Step2: convert sam file to bam and index bam
+# samtools view -bS --threads 10 ${targetdir}/${prefix}.sam -o ${targetdir}/${prefix}.bam
+# samtools index -@10 ${targetdir}/${prefix}.bam
