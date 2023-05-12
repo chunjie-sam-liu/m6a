a2d53ae722cca602a52cb6a7b1e2c8bc2035372e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-05-12 14:35:48 -0400

    clair3

--- test/clair3/clair3.sh
+++ test/clair3/clair3.sh
@@ -7,6 +7,8 @@
 # Number of input parameters
 # param=$#
 
+# ! APOBEC1YTH --------------------------------------------------------------------
+
 conda activate clair3
 
 MODEL_NAME="r941_prom_sup_g5014"
@@ -14,8 +16,44 @@ THREADS=30
 
 genomeref=/mnt/isilon/xing_lab/liuc9/refdata/bwa_index/Homo_sapiens.GRCh38.104.fa
 genomedir=/mnt/isilon/xing_lab/liuc9/refdata/bwa_index/
-targetdir=/scr1/users/liuc9/m6a/
-prefix=test4m.sorted.md
+inputdir=/home/liuc9/data/projdata/m6a/mapping
+outputdir=/home/liuc9/data/projdata/m6a/clair3
+prefix=APOBEC1YTH.sorted.md
+
+singularity exec \
+  -B ${genomedir},${inputdir},${outputdir} \
+  /home/liuc9/sif/clair3_latest.sif \
+  /opt/bin/run_clair3.sh \
+  --bam_fn=${inputdir}/${prefix}.bam \
+  --ref_fn=${genomedir}/Homo_sapiens.GRCh38.104.fa \
+  --threads=${THREADS} \
+  --platform="ont" \
+  --model_path="/opt/models/${MODEL_NAME}" \
+  --output=${outputdir}
+
+# ! test4m --------------------------------------------------------------------
+# conda activate clair3
+
+# MODEL_NAME="r941_prom_sup_g5014"
+# THREADS=30
+
+# genomeref=/mnt/isilon/xing_lab/liuc9/refdata/bwa_index/Homo_sapiens.GRCh38.104.fa
+# genomedir=/mnt/isilon/xing_lab/liuc9/refdata/bwa_index/
+# targetdir=/scr1/users/liuc9/m6a/
+# prefix=test4m.sorted.md
+
+# singularity exec \
+#   -B ${genomedir},${targetdir} \
+#   /home/liuc9/sif/clair3_latest.sif \
+#   /opt/bin/run_clair3.sh \
+#   --bam_fn=${targetdir}/${prefix}.bam \
+#   --ref_fn=${genomedir}/Homo_sapiens.GRCh38.104.fa \
+#   --threads=${THREADS} \
+#   --platform="ont" \
+#   --model_path="/opt/models/${MODEL_NAME}" \
+#   --output=${targetdir}
+
+# ! conda install clair3 failed  --------------------------------------------------------------------
 
 # /home/liuc9/tools/Clair3/run_clair3.sh \
 #   --bam_fn=${targetdir}/${prefix}.bam \
@@ -24,14 +62,3 @@ prefix=test4m.sorted.md
 #   --platform="ont" \
 #   --model_path="${CONDA_PREFIX}/bin/models/${MODEL_NAME}" \
 #   --output=${targetdir}
-
-singularity exec \
-  -B ${genomedir},${targetdir} \
-  /home/liuc9/sif/clair3_latest.sif \
-  /opt/bin/run_clair3.sh \
-  --bam_fn=${targetdir}/${prefix}.bam \
-  --ref_fn=${genomedir}/Homo_sapiens.GRCh38.104.fa \
-  --threads=${THREADS} \
-  --platform="ont" \
-  --model_path="/opt/models/${MODEL_NAME}" \
-  --output=${targetdir}
