993972eac4ae44120ce8cc058a76f1ac4ef87fab
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-05-12 21:05:27 -0400

    flare sailor

--- test/FLARE/FLARE.sh
+++ test/FLARE/FLARE.sh
@@ -7,3 +7,12 @@
 # Number of input parameters
 param=$#
 # ? we include with this release of FLARE an update of SAILOR to enable detection of all edit types.
+
+snakemake \
+  --snakefile /scr1/users/liuc9/tools/FLARE/workflow_sailor/Snakefile \
+  --configfile /home/liuc9/github/m6a/test/FLARE/sailor.json \
+  --verbose \
+  --use-singularity \
+  \
+  --singularity-args '--bind /scr1/users/liuc9 --bind /mnt/isilon/xing_lab/liuc9 --bind /mnt/isilon/xing_lab' \
+  -j1 # --singularity-args '--bind /mnt/isilon/xing_lab/liuc9/refdata/bwa_index/ --bind /scr1/users/liuc9/m6a --bind /scr1/users/liuc9/m6a --bind /mnt/isilon/xing_lab/liuc9/refdata/dbsnp/knownsnp/' \
