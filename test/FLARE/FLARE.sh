e24ba8d18cd3a26a4ebeda8a7328597c29a525fd
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-05-15 21:15:56 -0400

    peakcalling

--- test/FLARE/FLARE.sh
+++ test/FLARE/FLARE.sh
@@ -10,15 +10,24 @@ param=$#
 # ! real data --------------------------------------------------------------------
 
 # ! peakcalling --------------------------------------------------------------------
+# nohup snakemake \
+#   --profile /home/liuc9/github/m6a/test/FLARE/profiles/peakcaller/ \
+#   --configfile /home/liuc9/github/m6a/test/FLARE/peakcalling_real.json &
+
 nohup snakemake \
-  --profile /home/liuc9/github/m6a/test/FLARE/profiles/peakcaller/ \
-  --configfile /home/liuc9/github/m6a/test/FLARE/peakcalling_real.json &
+  --snakefile /scr1/users/liuc9/tools/FLARE/workflow_peakcaller/Snakefile \
+  --configfile /home/liuc9/github/m6a/test/FLARE/peakcalling_real.json \
+  --verbose \
+  --use-singularity \
+  --singularity-args '--bind /scr1/users/liuc9 --bind /mnt/isilon/xing_lab/liuc9 --bind /mnt/isilon/xing_lab' \
+  --singularity-prefix /mnt/isilon/xing_lab/liuc9/projdata/m6a/flare_singularity \
+  -j8 &
 
 # ! sailor --------------------------------------------------------------------
 
-nohup snakemake \
-  --profile /home/liuc9/github/m6a/test/FLARE/profiles/sailor/ \
-  --configfile /home/liuc9/github/m6a/test/FLARE/sailor_real.json &
+# nohup snakemake \
+#   --profile /home/liuc9/github/m6a/test/FLARE/profiles/sailor/ \
+#   --configfile /home/liuc9/github/m6a/test/FLARE/sailor_real.json &
 
 nohup snakemake \
   --snakefile /scr1/users/liuc9/tools/FLARE/workflow_sailor/Snakefile \
